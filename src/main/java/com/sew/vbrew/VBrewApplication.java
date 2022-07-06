ea6100e87ce4fe0093af81a5572909b9401868b9
udeshika-sewwandi <udeshika.sewwandi@cognite.com>
2022-07-06 20:25:15 +0530

    edit.

--- src/main/java/com/sew/vbrew/VBrewApplication.java
+++ src/main/java/com/sew/vbrew/VBrewApplication.java
@@ -1,7 +1,16 @@
 package com.sew.vbrew;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class VBrewApplication {
+
+  private static Logger logger = LoggerFactory.getLogger(VBrewApplication.class);
+
   public static void main(String[] args) {
-    System.out.println("I am running over the fence.");
+    int a = 2;
+    int b = 3;
+    int c = a * b;
+    logger.info("c {}", c);
   }
 }
